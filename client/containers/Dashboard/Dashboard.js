864ecbd70e5ac22c16a59363bda0a83641491663
Shankar Regmi <shankarregmi@gmail.com>
2017-01-28 14:57:06 +0545

    adds a dashboard after login is successful

---
+++ client/containers/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function() {
+  return (
+    <div className="container">
+      <h1>Welcome</h1>
+      <p>You are now loggedIn</p>
+    </div>
+  );
+}
